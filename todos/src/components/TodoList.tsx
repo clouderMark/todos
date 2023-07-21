1aa1611e6b10ff124312b4ae65b52b008453a11a
clouderMark <charnysheuski@yandex.by>
2023-07-21 18:47:10 +0300

    feat: make sorting data by filters

--- todos/src/components/TodoList.tsx
+++ todos/src/components/TodoList.tsx
@@ -2,13 +2,19 @@ import {Box} from '@mui/material';
 import {useAppSelector} from '../redux/hooks';
 import {selectTodo} from '../redux/todoListSlice';
 import TodoItem from './TodoItem';
+import {selectSortBy} from '../redux/sortBySlice';
 
 const TodoList = () => {
   const {todos} = useAppSelector(selectTodo);
+  const {hash: query} = useAppSelector(selectSortBy);
 
   return (
     <Box sx={{mt: 2}}>
-      {todos.length ? todos.map((todo) => <TodoItem key={todo.id} id={todo.id} />) : 'Cписок задач пуст'}
+      {query.length ? todos
+        .filter(({hash}) => !(query.every((el) => hash.indexOf(el))))
+        .map((todo) => (
+          <TodoItem key={todo.id} id={todo.id} />
+        )) : todos.map((todo) => <TodoItem key={todo.id} id={todo.id} />)}
     </Box>
   );
 };
