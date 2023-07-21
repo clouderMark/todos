d680c3f8acdc0552fdae1cb5f3e20ef762bd5eba
clouderMark <charnysheuski@yandex.by>
2023-07-21 17:07:12 +0300

    feat: add multi select for sorting items

--- todos/src/redux/store.ts
+++ todos/src/redux/store.ts
@@ -2,12 +2,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import {todoFormSlice} from './todoFormSlice';
 import {todoListSlice} from './todoListSlice';
 import {toastSlice} from './toastSlice';
+import {sortBySlice} from './sortBySlice';
 
 export const store = configureStore({
   reducer: {
     todoForm: todoFormSlice.reducer,
     todoList: todoListSlice.reducer,
     toast: toastSlice.reducer,
+    sortBy: sortBySlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
--- todos/src/views/Todolist.tsx
+++ todos/src/views/Todolist.tsx
@@ -1,12 +1,40 @@
-import {Container} from '@mui/material';
+import {useEffect} from 'react';
+import {Box, Container} from '@mui/material';
 import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
+import SortBy from '../components/SortBy';
+import {useAppDispatch} from '../redux/hooks';
+import {setNames} from '../redux/sortBySlice';
 
-const Todolist = () => (
-  <Container maxWidth={false}>
-    <TodoForm />
-    <TodoList />
-  </Container>
-);
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+const Todolist = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setNames(names));
+  }, []);
+
+  return (
+    <Container maxWidth={false}>
+      <TodoForm />
+      <Box sx={{display: 'flex', flexDirection: 'row-reverse'}}>
+        <SortBy />
+      </Box>
+      <TodoList />
+    </Container>
+  );
+};
 
 export default Todolist;
